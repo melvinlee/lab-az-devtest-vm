bc700fa6f0c18ce5a0f4625ad25c384e21a1753e
melvinlee <melvinlee@hotmail.com>
2018-07-30 20:39:37 +0800

    refactor merged variables.

--- vm-windows10/variables.tf
+++ vm-windows10/variables.tf
@@ -1,3 +1,21 @@
+variable "resource_group_name" {
+  type        = "string"
+  description = "Name of the azure resource group."
+  default     = "devlab-rg"
+}
+
+variable "resource_group_location" {
+  type        = "string"
+  description = "Location of the azure resource group."
+  default     = "southeastasia"
+}
+
+variable "environment" {
+  type        = "string"
+  description = "dev, test or production."
+  default     = "dev"
+}
+
 variable "vm_size" {
   type        = "string"
   description = "virtual machine size"
