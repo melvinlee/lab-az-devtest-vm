e5bbc13417cfee58c83a441231a60f87de394a44
melvinlee <melvinlee@hotmail.com>
2019-09-30 17:16:35 +0800

    add user to docker user group.

--- virtual-machines-extension/bootstrap.ps1
+++ virtual-machines-extension/bootstrap.ps1
@@ -14,6 +14,9 @@ choco install googlechrome -y
 # Utilities
 choco install everything /run-on-system-startup /desktop-shortcut -y
 
+# Add user to docker user group
+Add-LocalGroupMember -Group "docker-users" -Member "Developer"
+
 # Enable Hyper-V for Docker
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 
